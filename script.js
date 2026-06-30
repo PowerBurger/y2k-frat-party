0d2d34ef5ec4e96e503088efdb5249f1462974e3
PowerBurger <speedframegames@gmail.com>
2026-06-30 22:48:23 +0200

    jj

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 console.log("DIN MAMMAAA")
-import Swal from "sweetalert2"
+//import Swal from "sweetalert2"
 
 const scriptURL = "https://script.google.com/macros/s/AKfycbyEtKwlX8akvz0ChmOil5ruIapsmaKyUUot1bFbuSOjt2vyiziCEudDq6YQdOKmrp59/exec";
 
@@ -8,10 +8,14 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     var formData = new FormData(form)
     console.log("trying fetch")
+    Swal.fire({title: "Sending", text: "Just one moment...", showConfirmButton: false})
     fetch(scriptURL, { method: "POST", body: formData})
     .then((response) => {
-        Swal.fire({title: "NICE", text: "See you there!", icon: "success"});})
+        Swal.fire({title: "Signed Up", text: "You're all set, see you there :D", icon: "success"});
+        //window.alert("Done! See you there :D");
+    })
     .catch((error) => {
         Swal.fire({title: "Something went wrong!", text: "This guy must not know how to code...", icon: "error"})
+        //window.error("Something went wrong!");
     })
 })
